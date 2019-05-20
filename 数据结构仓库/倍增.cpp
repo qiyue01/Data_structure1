4fe1b579c26b700e618e3228506b43a5cf9c50ea
qiyue7 <zhangboqi007@live.com>
2019-05-21 00:21:48 +0800

    0

--- 数据结构仓库/倍增.cpp
+++ 数据结构仓库/倍增.cpp
@@ -56,3 +56,23 @@ void  build(int root, int n)
 			if (father[k][i])
 				father[k + 1][i] = father[k][father[k][i]];
 }
+int LCA(int u, int v)
+{
+	if (depth[u] > depth[v])
+		return LCA(v, u);
+	if (depth[u] != depth[v]) {   //先将v上提，使两个结点在同一深度上
+		for (int k = 20 - 1; k >= 0; k--) {
+			if (depth[v] - (1 << k) >= depth[u]) //1<<k即2^k  v上提后不能在u的上方 
+				v = father[k][v];
+		}
+	}
+	if (u == v) return u;  //如果在同一结点上，直接得LCA 
+	for (int k = max2 - 1; k >= 0; k--) {  //二分法一点点缩小范围 
+		if (father[k][u] == 0) continue;  //不能越界
+		if (father[k][u] != father[k][v]) {  //如果fa不同，将两点都上提 
+			u = father[k][u];
+			v = father[k][v];
+		}
+	}
+	return father[0][u];
+}
